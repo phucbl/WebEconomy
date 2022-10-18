1eccbf257e055afc5c456f8f83c7d2f765f17b64
phucbl <48372474+phucbl@users.noreply.github.com>
2022-10-18 09:34:39 +0700

    Add files via upload

---
+++ src/main/java/com/example/webeconomy/SecurityConfigure.java
@@ -0,0 +1,17 @@
+package com.example.webeconomy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.Customizer;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+// import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfigure {
+
+    
+    
+    
+
+}
---
+++ src/main/java/com/example/webeconomy/WebEconomyApplication.java
@@ -0,0 +1,13 @@
+package com.example.webeconomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEconomyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEconomyApplication.class, args);
+	}
+
+}
